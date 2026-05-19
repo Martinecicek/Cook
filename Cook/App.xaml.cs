052ddd93540c914a7a37dbd4a68d668ccc1790be
martin.krenek <martin.krenek@PC-U13-08>
2026-05-19 08:36:15 +0200

    Přidejte soubory projektu.

---
+++ Cook/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Cook
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
